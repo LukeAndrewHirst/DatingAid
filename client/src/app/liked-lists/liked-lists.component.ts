fdf055901569267fcac6e6c870de309b2af3f837
LukeAndrewHirst <luke.hirst.la@gmail.com>
2024-10-28 08:03:31 +0200

    Implemented angular routing & changed the them

---
+++ client/src/app/liked-lists/liked-lists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liked-lists',
+  standalone: true,
+  imports: [],
+  templateUrl: './liked-lists.component.html',
+  styleUrl: './liked-lists.component.css'
+})
+export class LikedListsComponent {
+
+}
--- client/src/app/navigation/navigation.component.ts
+++ client/src/app/navigation/navigation.component.ts
@@ -2,27 +2,33 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
 export class NavigationComponent {
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   accountservice = inject(AccountService);
 
   model: any = {};
 
   login(){
     this.accountservice.login(this.model).subscribe({
-      next: response => {console.log(response);},
-      error: error => {console.log(error);}
+      next: response => {this.router.navigateByUrl('/members');},
+      error: error => {this.toastr.error(error.error); console.log(error)}
     });
   }
 
   logout(){
-    this.accountservice.logout()
+    this.accountservice.logout();
+    this.router.navigateByUrl('/');
   }
 }
